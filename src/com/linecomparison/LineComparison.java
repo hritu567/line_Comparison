8c4247fb3bcce451c79c978678d35008698f535a
hritu567 <hritumog96@gmail.com>
2020-11-17 20:56:34 +0530

    Added : calculating length

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,10 +1,20 @@
 package com.linecomparison;
-
-public class LineComparison {
-
+import java.util.Scanner;
+//uc1
+public class LineComparison { 
+	//calculating length
 	public static void main(String[] args) {
-	 System.out.println("Welcome to line comparison computation program");
+		double lineLength = 0;
+		Scanner myObj = new Scanner(System.in); 
+	    System.out.println("Enter first number, second number, third number and fourth number");
+	    double x1=myObj.nextDouble();
+	    double y1=myObj.nextDouble();
+	    double x2=myObj.nextDouble();
+	    double y2=myObj.nextDouble();
 
+	lineLength = (Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
+
+		System.out.println("Length of a line is : " + lineLength);
 	}
 
 }
