0f39734269feab177e1248f28eda2ccb26a531da
hritu567 <hritumog96@gmail.com>
2020-11-17 22:57:03 +0530

    Added : equality checking

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -1,20 +1,38 @@
 package com.linecomparison;
 import java.util.Scanner;
-//uc1
+
+//uc2
 public class LineComparison { 
 	//calculating length
-	public static void main(String[] args) {
-		double lineLength = 0;
-		Scanner myObj = new Scanner(System.in); 
-	    System.out.println("Enter first number, second number, third number and fourth number");
-	    double x1=myObj.nextDouble();
-	    double y1=myObj.nextDouble();
-	    double x2=myObj.nextDouble();
-	    double y2=myObj.nextDouble();
-
-	lineLength = (Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))));
-
-		System.out.println("Length of a line is : " + lineLength);
+	private static double calculateLength() 
+	{
+		Scanner sc = new Scanner(System.in);
+		double x1,x2,y1,y2;
+		System.out.println("Enter the values x1,x2,y1,y2");
+		x1=sc.nextInt();
+		x2=sc.nextInt();
+		y1=sc.nextInt();
+		y2=sc.nextInt();
+		System.out.println("first point " + x1 + "\n second point " + y1 + "\n third point " + x2 + "\n fourth point " + y2);
+		double lenOfLine =Math.sqrt((x2 - x1) * (x2 - x1) + (y2- y1) * (y2 - y1));
+		return lenOfLine;
+	}
+	//Checking two line are equal or not
+	private static void equalityCheck(double l1, double l2) 
+	{ 
+		if(l1 == l2)
+			System.out.println("Both Lines are equal");
+		else
+	    	System.out.println("Both Lines are not equal ");
 	}
 
+
+	public static void main(String[] args) {
+		System.out.println("Enter Line 1 points");
+		double line1 = calculateLength();
+		System.out.println("Enter Line 2 points");
+		double line2 = calculateLength();
+		System.out.println("Length of line1 " + line1 + "\n Length of line2 " + line2);
+		equalityCheck(line1, line2);
+}
 }
